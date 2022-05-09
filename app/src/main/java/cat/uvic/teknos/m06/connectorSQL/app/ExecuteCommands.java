6b6cdc531c030bdde1abf3b68b90cf3a2e2bef7f
Abdelkarim El Bouzzaoui del Moral <abdelkarim.el@uvic.cat>
2022-05-09 17:21:06 +0200

    add connection error to log

--- app/src/main/java/cat/uvic/teknos/m06/connectorSQL/app/ExecuteCommands.java
+++ app/src/main/java/cat/uvic/teknos/m06/connectorSQL/app/ExecuteCommands.java
@@ -2,38 +2,82 @@ package cat.uvic.teknos.m06.connectorSQL.app;
 
 import cat.uvic.teknos.m06.connectorSQL.app.Exception.ExecuteCommandException;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ExecuteCommands implements SchemaLoader{
     public final String[] commands;
     private final ConnectionProperties conProps;
-
+    private static String pathLog = "C:\\tmp\\executeLog.txt";
     public ExecuteCommands(XmlSchemaLoader schema, ConnectionProperties conn) {
         this.commands = schema.getSchema().getCommands();
         this.conProps = conn;
     }
 
     @Override
-    public void load() throws ExecuteCommandException{
+    public void load() throws ExecuteCommandException,SQLException {
         var error = "";
-        try{
-            var con = DriverManager.getConnection(conProps.getUrl(),conProps.getUsername(), conProps.getPassword());
-            var statement = con.createStatement();
 
-            for(int i = 0; commands.length> i;i++ ){
-                try{
-                    statement.executeUpdate(commands[i]);
-                }catch (SQLException e) {
-                    error += "Error en ejecutar Comando nº " + i+1 + e.getMessage();
-                }
-            }
-            if(error.length() != 0){
-                throw new ExecuteCommandException(error);
-            }
-        } catch (SQLException e) {
+        var con = getConnection();
+        var statement = con.createStatement();
 
+        for(int i = 0; commands.length> i;i++ ){
+            error = executeCommand(error, statement, i);
         }
 
+        if(logExist()){
+            throw new ExecuteCommandException("has been errors on Execute, view log on: "+ pathLog);
+        }
+    }
+
+    private String executeCommand(String error, Statement statement, int i) {
+        try{
+            statement.executeUpdate(commands[i]);
+        }catch (SQLException e) {
+            var num = i +1;
+            var mes = "\n Error en ejecutar Comando nº " + num + e.getMessage();
+            error += mes;
+            addToLog(mes);
+        }
+        return error;
+    }
+
+    private Connection getConnection()  {
+        try {
+            return DriverManager.getConnection(conProps.getUrl(), conProps.getUsername(), conProps.getPassword());
+        } catch (SQLException e) {
+            addToLog("\n " +e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void addToLog(String mess){
+        File log = new File(pathLog);
+        try{
+            if (!log.exists()) {
+                log.createNewFile();
+            }
+            FileWriter fw = new FileWriter(log);
+            BufferedWriter writter = new BufferedWriter(fw);
+            writter.write(mess);
+
+            writter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean logExist(){
+        File log = new File(pathLog);
+
+        return log.exists();
     }
 }
--- app/src/test/java/cat/uvic/teknos/m06/connectorSQL/app/ExecuteCommandsTest.java
+++ app/src/test/java/cat/uvic/teknos/m06/connectorSQL/app/ExecuteCommandsTest.java
@@ -2,6 +2,9 @@ package cat.uvic.teknos.m06.connectorSQL.app;
 
 import cat.uvic.teknos.m06.connectorSQL.app.Exception.ExecuteCommandException;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExecuteCommandsTest {
@@ -14,5 +17,19 @@ class ExecuteCommandsTest {
             var singleCommandSchema = new ExecuteCommands(schema,conn);
 
         assertDoesNotThrow(()->singleCommandSchema.load());
+    }
+
+    @Test
+    void throwExecuteCommandsException(){
+        ConnectionProperties conn = new ConnectionProperties("jdbc:mysql://localhost:3306/mysql","root","");
+        XmlSchemaLoader schema = new XmlSchemaLoader("src/test/resources/xml/schema.xml");
+        var executeCommands = new ExecuteCommands(schema,conn);
+
+        assertThrows(ExecuteCommandException.class,()->executeCommands.load());
+    }
+
+    @Test
+    void throwExceptionErrorLog(){
+
     }
 }
